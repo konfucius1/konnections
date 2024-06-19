65b653367560ef884309503495c2c8e6d7a37e62
Thanakon <kimsanthanakon@gmail.com>
2024-06-19 19:56:50 +1000

    feat: add new object type for category of words that includes tricky level

--- src/data.ts
+++ src/data.ts
@@ -8,3 +8,19 @@ export const categories: Categories = {
   'Our special places': ['Park', 'Beach', 'Bed', 'Garden'],
   'Classic planet cooking routine': ['Wash', 'Dry', 'Salt', 'Pepper'],
 };
+
+export const data = [
+  {
+    category: 'Favorite activities together',
+    words: ['Travel', 'Walk', 'Dinner', 'Movie'],
+  },
+  {
+    category: 'Our signature handshake',
+    words: ['Shake', 'Gang', 'Bump', 'Wrestle'],
+  },
+  { category: 'Our special places', words: ['Park', 'Beach', 'Bed', 'Garden'] },
+  {
+    category: 'Classic planet cooking routine',
+    words: ['Wash', 'Dry', 'Salt', 'Pepper'],
+  },
+];
