bef9acc0f41369e9041057a8b9137981971d6bbd
Thanakon <kimsanthanakon@gmail.com>
2024-06-19 23:33:33 +1000

    fix: add static type checks for format time function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -19,9 +19,9 @@ export function getAllWords() {
   return Object.values(categories).flat();
 }
 
-export function formatTime(time) {
+export function formatTime(time: number) {
   const getSeconds = `0${time % 60}`.slice(-2);
-  const minutes = `${Math.floor(time / 60)}`;
+  const minutes = parseInt(`${Math.floor(time / 60)}`, 10);
   const getMinutes = `0${minutes % 60}`.slice(-2);
   const getHours = `0${Math.floor(time / 3600)}`.slice(-2);
 
